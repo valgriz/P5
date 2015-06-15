aca0e829b3c364c2c058922b7306d5413c515550
Steven Marturano <steven.marturano@gmail.com>
2015-06-15 16:25:43 -0400

    initial commit

---
+++ desktop/src/com/valgriz/plunge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.valgriz.plunge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.valgriz.plunge.PenguinPlunge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PenguinPlunge(), config);
+	}
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+Project Penguin Plunge Paradise Port
+P5 - Started on 6/15/15 4:07 PM by Steven Marturano
